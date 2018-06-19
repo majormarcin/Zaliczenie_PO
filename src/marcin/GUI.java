9eb0a522db98fe8a2956aa99153dc6dbefa27098
MajorMarcin <zelkowski.m@gmail.com>
2018-06-19 22:04:05 +0200

    v1.0

--- src/marcin/GUI.java
+++ src/marcin/GUI.java
@@ -8,13 +8,11 @@ import javax.swing.event.*;
 public class GUI extends JFrame {
 
 	private JTextField x1,x2,x3,y1,y2,y3;
-	
 	private JLabel info= new JLabel(" Info ");
 	public String txt="";
 	//deklaracja pozycji w menu przed listenerem ktory wymaga obiektu
 	private JMenuItem Zamknij;
 	private JMenuItem Zapisz;
-	
 	//listener dla eventu klikniecia pozycji w menu
 	private ActionListener al = new ActionListener(){
 	public void actionPerformed(ActionEvent e) {
@@ -23,7 +21,6 @@ public class GUI extends JFrame {
 	else if(e.getSource() == Zapisz)
 		zapis(txt);
 		}
-	
 	};
 
 	//zapis z wyborem miejsca 
@@ -41,7 +38,6 @@ public class GUI extends JFrame {
 	        }
 	    }
 	}
-	
 	// zapis bez wyboru do pliku 
 //	public static void zapis(String text) 
 //			throws IOException
@@ -56,7 +52,7 @@ public class GUI extends JFrame {
 	
 	//klasa z neta ale świetnie działa jako Listener :D
 	class MyDocumentListener implements DocumentListener {
-	    public void insertUpdate(DocumentEvent e) {
+	    public void insertUpdate(DocumentEvent e) {	    	
 	    	punkt();
 	    }
 	    public void removeUpdate(DocumentEvent e) {
@@ -66,18 +62,24 @@ public class GUI extends JFrame {
 	        //Plain text components do not fire these events
 	    	punkt();
 	    }
-	    
-	    
 	}
+	int delta;
 	public void punkt(){
-			String txt="Punkty ";
-			info.setText(txt+" P1 ("+x1.getText()+","+y1.getText()+") "+" P2 ("+x2.getText()+","+y2.getText()+") "+" P3 ("+x3.getText()+","+y3.getText()+") ");
-			this.txt=" P1 ("+x1.getText()+","+y1.getText()+") "+" P2 ("+x2.getText()+","+y2.getText()+") "+" P3 ("+x3.getText()+","+y3.getText()+") ";
-		}
-	
+		//wywala bledy ale dizala
+		delta = Integer.parseInt(x1.getText())*Integer.parseInt(y2.getText()) + Integer.parseInt(x2.getText())*Integer.parseInt(y3.getText()) + Integer.parseInt(x3.getText())*Integer.parseInt(y1.getText()) - Integer.parseInt(x3.getText())*Integer.parseInt(y2.getText()) - Integer.parseInt(x1.getText())*Integer.parseInt(y3.getText()) - Integer.parseInt(x2.getText())*Integer.parseInt(y1.getText()); 
+        if(delta == 0){
+        	info.setText("(" + x1.getText() + "," + y1.getText() + ")   (" + x2.getText() + "," + y2.getText() + ")   (" + x3.getText() + "," + y3.getText() + ")   Punkty są współliniowe");
+        	this.txt="(" + x1.getText() + "," + y1.getText() + ")   (" + x2.getText() + "," + y2.getText() + ")   (" + x3.getText() + "," + y3.getText() + ")   Punkty sa wspolliniowe";
+        }
+        else{
+        	info.setText("(" + x1.getText() + "," + y1.getText() + ")   (" + x2.getText() + "," + y2.getText() + ")   (" + x3.getText() + "," + y3.getText() + ")   Punkty nie są współliniowe");
+        	this.txt="(" + x1.getText() + "," + y1.getText() + ")   (" + x2.getText() + "," + y2.getText() + ")   (" + x3.getText() + "," + y3.getText() + ")   Punkty nie sa wspolliniowe";
+        }
+		//info.setText(txt+" P1 ("+x1.getText()+","+y1.getText()+") "+" P2 ("+x2.getText()+","+y2.getText()+") "+" P3 ("+x3.getText()+","+y3.getText()+") ");
+		//this.txt=" P1 ("+x1.getText()+","+y1.getText()+") "+" P2 ("+x2.getText()+","+y2.getText()+") "+" P3 ("+x3.getText()+","+y3.getText()+") ";
+	}
 
-	public GUI() {
-				
+	public GUI() {		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(null);
 		//zablokowane zeby layout nie wyglądal brzydnko
@@ -86,24 +88,18 @@ public class GUI extends JFrame {
 		setTitle("Marcin Zelkowski");
 		//jego rozmiar(szer,wys)
 		setSize(520, 700);
-		
 		setVisible(true);
 		//deklaracja obiektu menu
-		JMenuBar mb = new JMenuBar();
-		
+		JMenuBar mb = new JMenuBar();	
 		//deklaracja pierwszego obiektu w menu
-		JMenu menu1 = new JMenu("Plik");
-		
+		JMenu menu1 = new JMenu("Plik");	
 		//dodanie tego obiektu do menu 
-		mb.add(menu1);
-		
+		mb.add(menu1);	
 		//deklaracja pozycji w menu
 		//on wymaga listenera dla eventu wiec byl zadeklarowany wczesniej
 		Zapisz = new JMenuItem("Zapisz");
 		Zamknij = new JMenuItem("Zamknij");
-		
 		//dodanie 
-		
 		menu1.add(Zapisz);
 		menu1.add(Zamknij);
 		setJMenuBar(mb);//pojawianie menu
@@ -113,17 +109,15 @@ public class GUI extends JFrame {
 		//koniec menu początek pól
 		 
 		//pola na dane są zadeklarowane wcześniej ale i tak wymagają stworzenia ich jako obiekty
-		x1 = new JTextField("x");
+		x1 = new JTextField();
 		x2 = new JTextField();
 		x3 = new JTextField();
-		//x1.addActionListener(new MyTextActionListener());
-		    
+		//x1.addActionListener(new MyTextActionListener());  
 		//x1.getDocument().putProperty("owner", x1);
 		
-		y1 = new JTextField("y");
+		y1 = new JTextField();
 		y2 = new JTextField();
 		y3 = new JTextField();
-		
 		//listenery dla zmian w polach
 		x1.getDocument().addDocumentListener(new MyDocumentListener());
 		x2.getDocument().addDocumentListener(new MyDocumentListener());
@@ -176,12 +170,9 @@ public class GUI extends JFrame {
 	            //ustala kolor markera
 	            g.setColor(Color.WHITE);
 	            //g.fillRect(0, 0, getWidth(), getHeight());
-	            
-	            g.setColor(Color.BLACK);
-	            
+	            g.setColor(Color.BLACK); 
 	            g.drawLine(0, 150, 500, 150);//x
 	            g.drawLine(250, 0, 250, 300);//y
-	            
 	            //strzałki osi
 	            //y
 	            g.drawLine(250, 0, 240, 10);
@@ -196,35 +187,32 @@ public class GUI extends JFrame {
 	            		g.drawLine(245, i, 255, i);//2
 	            	}
 	            }
-	            
 	            for (int i=10;i<=500;i=i+10) {
 	            	g.drawLine(i, 147, i, 153);//x
 	            	if ((i % 50) == 0) {
 	            		g.drawLine(i, 145, i, 155);//x
 	            	}
 	            }
-
 	            g.setColor(Color.RED);
-	            g.drawOval(25, 5, 5, 5);
+	            //g.drawOval(25, 5, 5, 5);
 	        };
 	    };
-	    
 	    int pkt;
 	    Component canvas2 = new Component() {
+	    	public void paintComponent2(Graphics g, int ws1,int ws2) {
+	            g.setColor(Color.RED);
+	            g.drawOval(ws1, ws2, 5, 5);
+	        };
 		};
-	    
 	    canvas.setBounds(10, 300, 490, 300);//dorysowuje canva
 		add(canvas);
-		
-		
+		canvas2.setBounds(10, 300, 490, 300);//dorysowuje canva
+		add(canvas2);
 		}
-	
 		public int wspolrzedne(int liczba) {
 			if(liczba>=0) {
 				return 250+liczba;
 			}
 			return liczba; 
 		}
-	
-	
 }
